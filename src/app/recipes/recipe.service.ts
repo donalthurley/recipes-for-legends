30682d5876926adfe0c4419884813cb4ec196275
Donal Hurley <donalthurley@gmail.com>
2020-09-20 18:10:10 +0100

    adding services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,68 @@
+
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
+
+    private recipes: Recipe[] = [
+    new Recipe('Pancakes', 'Pancakes are good for you.',
+     // tslint:disable-next-line:max-line-length
+     '../../assets/pexels-photo-264727.jpeg', // tea-cake-cafe-desserts-162827.jpeg
+
+    [
+        new Ingredient('Flour', 1),
+        new Ingredient('Egg', 1)
+    ]),
+
+     new Recipe('Waffles', 'Waffles are good for you.',
+     // tslint:disable-next-line:max-line-length
+     '../../assets/pexels-photo-227432.jpeg',
+
+    [
+        new Ingredient('Flour', 1),
+        new Ingredient('Milk', 3),
+        new Ingredient('Oil', 2)
+    ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+      // tslint:disable-next-line:max-line-length
+      return this.recipes.slice(); // Returns a new array which is an exact copy of the recipes array (as opposed to providing a reference to the recipes).
+  }
+
+  getRecipe(index: number) {
+      return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,45 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
+
+export class ShoppingListService {
+    ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Egg', 1),
+        new Ingredient('Milk', 0.5)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        // This is fine, but an event will get emitted for each ingredient.
+        /* for (let i = 0; i < ingredients.length; i++) {
+            this.addIngredient(ingredients[i]);
+        }*/
+
+        this.ingredients.push(...ingredients); // ES6 spread.
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+}
